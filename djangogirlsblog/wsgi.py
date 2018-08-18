8232fbca2883726e5420d44df21ccc8fdb50fcb7
Kat <sobylmine@gmail.com>
2018-08-18 13:04:35 +1000

    Initial commit :tada:

---
+++ djangogirlsblog/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for djangogirlsblog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangogirlsblog.settings")
+
+application = get_wsgi_application()
